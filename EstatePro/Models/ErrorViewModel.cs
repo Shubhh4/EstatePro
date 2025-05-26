aa0b6d4f54632c503df572033c7e387eacdbf6da
Shubham Pathak <95904081+Shubhh4@users.noreply.github.com>
2025-05-26 23:19:19 +0530

    initial commit

---
+++ EstatePro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EstatePro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
